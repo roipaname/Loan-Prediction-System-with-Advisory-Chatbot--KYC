7df37126030c345c7600e8939c0c75f27a1acb23
roipaname <roipaname@gmail.com>
2026-03-28 14:47:53 +0200

    modified settinsg

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,22 @@ from loguru import logger
 from typing import Dict
 
 load_dotenv()
+#==================================
+# Project root directory and Paths
+#==================================
+
+BASE_DIR= Path(__file__).resolve().parent.parent
+DATA_DIR=BASE_DIR / 'data'
+RAW_DATA_DIR= DATA_DIR / 'raw'
+PROCESSED_DATA_DIR= DATA_DIR / 'processed'
+MODELS_DIR= BASE_DIR / 'models'
+DATABASE_DIR=BASE_DIR /'database'
+SRC_DIR=BASE_DIR /'src'
+AI_ADVISOR_DIR= SRC_DIR/'ai_advisor'
+CLASSIFIER_DIR=SRC_DIR/'classifier'
+TF_IDF_DIR=SRC_DIR/'tf_idf'
+SCRIPTS_DIR=BASE_DIR/'scripts'
+
 
 
 
