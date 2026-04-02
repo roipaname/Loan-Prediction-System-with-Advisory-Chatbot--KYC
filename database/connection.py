2299761adf170f4dead0278c3f69ed4e6c28b74b
roipaname <roipaname@gmail.com>
2026-04-02 18:55:32 +0200

    creating connection

--- database/connection.py
+++ database/connection.py
@@ -3,3 +3,8 @@ from sqlalchemy import create_engine, session,sessionmaker
 from loguru import logger
 
 from config.settings import DB_URL
+
+
+class Connection:
+    def __int__(self):
+        pass
